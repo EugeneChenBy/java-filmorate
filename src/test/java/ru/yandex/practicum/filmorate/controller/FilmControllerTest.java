d33b8680c6c50f06b04acc4f80cece7ee0db119e
EugeneChenBy <misyuchenko@mail.ru>
2023-05-29 20:13:30 +0300

    ТЗ-11: правки для прохождения PR-а

--- src/test/java/ru/yandex/practicum/filmorate/controller/FilmControllerTest.java
+++ src/test/java/ru/yandex/practicum/filmorate/controller/FilmControllerTest.java
@@ -1,22 +1,22 @@
 package ru.yandex.practicum.filmorate.controller;
 
 import lombok.RequiredArgsConstructor;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+//import org.junit.jupiter.api.BeforeEach;
+//import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.jdbc.core.JdbcTemplate;
 import ru.yandex.practicum.filmorate.model.Film;
 import ru.yandex.practicum.filmorate.service.FilmService;
-import ru.yandex.practicum.filmorate.service.UserService;
-import ru.yandex.practicum.filmorate.storage.FilmDBStorage;
-import ru.yandex.practicum.filmorate.storage.UserDBStorage;
+//import ru.yandex.practicum.filmorate.service.UserService;
+//import ru.yandex.practicum.filmorate.storage.FilmDBStorage;
+//import ru.yandex.practicum.filmorate.storage.UserDBStorage;
 
-import java.time.Duration;
-import java.time.LocalDate;
+//import java.time.Duration;
+//import java.time.LocalDate;
 
-import static org.junit.jupiter.api.Assertions.*;
+//import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 @RequiredArgsConstructor(onConstructor_ = @Autowired)
