d4ad8af045dff0e23f3216ac12baabe5f03059ff
EugeneChenBy <misyuchenko@mail.ru>
2023-05-30 13:50:01 +0300

    ТЗ-11: дописал интегр тесты для получения жанров и рейтингов

--- src/test/java/ru/yandex/practicum/filmorate/storage/GenreStorageTest.java
+++ src/test/java/ru/yandex/practicum/filmorate/storage/GenreStorageTest.java
@@ -7,7 +7,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
 import ru.yandex.practicum.filmorate.model.Genre;
-import ru.yandex.practicum.filmorate.model.MPA;
 
 import java.util.List;
 
